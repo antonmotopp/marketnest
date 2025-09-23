5c795252df632f590a6469653b7a167b9cf7805c
Anton <anton.merkotun@motopp.nl>
2025-09-23 13:22:41 +0200

    feat(main): update middleware

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://marketnest-client.vercel.app"
+        "https://marketnest.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
