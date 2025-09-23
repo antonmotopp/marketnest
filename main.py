1cfd7a81bb10c878eda51ca3c8fbfef7bf78fe57
Anton <anton.merkotun@motopp.nl>
2025-09-23 13:20:38 +0200

    feat(main): update middleware

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ app.include_router(chat.router, prefix="/chat", tags=['websockets'])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://marketnest-client.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
